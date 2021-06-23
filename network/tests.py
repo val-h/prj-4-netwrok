26a71d990e4fa0d2b582db2a7a3cfa53890b0f91
val-h <valentin97mirchev@gmail.com>
2021-06-23 14:13:47 +0100

    4 new basic test cases for post and user models

--- network/tests.py
+++ network/tests.py
@@ -1,3 +1,33 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+
+from .models import Post, User
+
 
 # Create your tests here.
+class PostTestCase(TestCase):
+
+    def setUp(self):
+        # Load the test Posts
+        u1 = User.objects.create(
+            username='test',
+            password='testpass123',
+            email='email@test.com')
+
+        p1 = Post.objects.create(
+            op=u1,
+            content='test content')
+
+    def test_post_user(self):
+        post = Post.objects.get(id=1)
+        user = post.op
+        self.assertEqual(user.id, 1)
+
+    def test_post_content(self):
+        post = Post.objects.get(id=1)
+        self.assertEqual(post.content, 'test content')
+
+    def test_user_posts(self):
+        user = User.objects.get(id=1)
+        user_posts = user.posts.all()
+        self.assertEqual(len(user_posts), 1)
+        self.assertEqual(user_posts[0].content, 'test content')
